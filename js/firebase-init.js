e1c4e5d2b2bf682a4e76499a00cd47bdc4415691
se-miraz <165878797+se-miraz@users.noreply.github.com>
2024-12-18 13:46:24 +0900

    Update firebase-init.js

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -1,9 +1,17 @@
 // firebase-init.js
+// firebase-init.js
 const firebaseConfig = {
-  // 여기에 Firebase 설정을 넣으세요
+  apiKey: "AIzaSyBkTJzSymd5o7hOGFjtQjRzA8BWY09jX3A",
+  authDomain: "file-63e65.firebaseapp.com",
+  projectId: "file-63e65",
+  storageBucket: "file-63e65.appspot.com",
+  messagingSenderId: "546982460468",
+  appId: "1:546982460468:web:696af12875e5d1311ede61",
+  measurementId: "G-9XE2RR3B1G"
 };
 
 firebase.initializeApp(firebaseConfig);
 
 // 전역 변수로 Firestore 인스턴스 생성
 window.db = firebase.firestore();
+
